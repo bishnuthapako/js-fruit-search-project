ef3bea73ead6e39140506a9648b19019d090f156
bishnu.email.usa@gmail.com <bishnu.email.usa@gmail.com>
2023-05-15 09:36:04 -0400

    corrected the sentences

--- script.test.js
+++ script.test.js
@@ -1,6 +1,6 @@
 describe("Search the fruits by fruit name", function() {
   
-    it("returns the fruit suggestions if it matches the string passed", () => {
+    it("returns the fruit if it matches the string passed", () => {
       expect(search('Banana')).toEqual(['Banana']);
     });
     it("return empty array [] if you pass number", function(){
